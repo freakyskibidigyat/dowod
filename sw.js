9f98dd59178ae2be04b8d4163432d758747ef6f0
Michael8907 <marti.sun.123@gmail.com>
2025-04-12 15:26:39 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,21 @@
+const CACHE_NAME = "my-pwa-cache-v1";
+const FILES_TO_CACHE = [
+  "/dowod/",  // Główna strona aplikacji
+  "/dowod/index.html",  // Strona index
+  "/dowod/icon-192.png",  // Ikona 192x192
+  "/dowod/icon-512.png"  // Ikona 512x512
+];
+
+// Instalacja Service Workera i zapisanie plików w cache
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open("app-cache").then((cache) => {
-      return cache.addAll([
-        "/",
-        "/index.html",
-        "/style.css",
-        "/script.js",
-        "/icon-192.png",
-        "/icon-512.png"
-      ]);
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
     })
   );
 });
 
+// Obsługa żądań - zwracanie plików z cache
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
